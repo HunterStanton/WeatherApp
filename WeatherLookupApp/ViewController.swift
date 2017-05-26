26a7f777af8650a723e45a9a522277a1c8241489
Hunter_Stanton <ihatecompvir@icloud.com>
2017-05-26 19:12:47 -0400

    Added unwrapping of LocationTextField text

--- WeatherLookupApp/ViewController.swift
+++ WeatherLookupApp/ViewController.swift
@@ -14,6 +14,7 @@ import UIKit
 /// The main view controller for the weather app.
 class WeatherViewController: UIViewController {
     
+    // MARK: - IBOutlet declarations
     
     /// The view that shows the icon of the current weather conditions.
     @IBOutlet weak var WeatherIconView: UIImageView!
@@ -24,7 +25,7 @@ class WeatherViewController: UIViewController {
     /// A label that helps the user determine what the input field below is for.
     @IBOutlet weak var LocationNameLabel: UILabel!
     
-    /// The text field that the user will input the locationt to lookup into.
+    /// The text field that the user will input the location to lookup into.
     @IBOutlet weak var LocationTextField: UITextField!
     
     /// The button that initiates the weather conditions lookup on press.
@@ -32,19 +33,48 @@ class WeatherViewController: UIViewController {
     
     
     
-
+    // MARK: - View state functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Setup rounded corners on WeatherConditionsView
+        // Setup rounded corners on WeatherConditionsView by modifying the CALayer of the view
         WeatherConditionsView.layer.cornerRadius = 5.0
         WeatherConditionsView.layer.borderColor = UIColor.blue.cgColor
         WeatherConditionsView.layer.borderWidth = 2.0
+        
+        LocationTextField.layer.cornerRadius = 5.0
+        LocationTextField.layer.borderColor = UIColor.blue.cgColor
+        LocationTextField.layer.borderWidth = 2.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - IBOutlet actions
+    
+    @IBAction func LookupPressed(_ sender: Any) {
+        guard let text = LocationTextField.text else
+        {
+            print("Error unwrapping location name!")
+            return
+        }
+        
+        if (text.isEmpty)
+        {
+            print("User did not enter a location.")
+            return
+        }
+    }
+    
+    // MARK: - Touch related functions
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Dismiss the keyboard when anywhere but the keyboard is touched, so that the user can dismiss the keyboard on all devices
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
     }
 
 
